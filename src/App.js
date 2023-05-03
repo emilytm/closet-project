1e2b320f93de423171b27aa9e2206eec0492ccba
emilytm <113457923+emilytm@users.noreply.github.com>
2023-05-03 10:36:46 -0400

    updated header component and rendered in App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './assets/logo.svg';
 import './styles/App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          CAPSULE CLOSET (WIP)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          PREVIEW
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
